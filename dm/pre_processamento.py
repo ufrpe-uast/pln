2014f8ae467aeb3d7ccac6eaa0f4c074a292f536
David Pinheiro <davepinh@gmail.com>
2018-09-09 04:29:13 -0300

    Função para remover stopwords criada

--- dm/pre_processamento.py
+++ dm/pre_processamento.py
@@ -1,5 +1,21 @@
 from nltk.tokenize.treebank import TreebankWordTokenizer
+from nltk.corpus import stopwords
+
+# tokenizar_string : string -> function -> [string]
+def tokenizar_string(string, tokenizer=TreebankWordTokenizer()):
+    return tokenizer.tokenize(string)
 
 # tokenizar : [string] -> function -> [[string]]
 def tokenizar(lst, tokenizer=TreebankWordTokenizer()):
-    return [tokenizer.tokenize(x) for x in lst]
+    return [tokenizar_string(x, tokenizer) for x in lst]
+
+# rem_stopwords : string -> [string] -> string
+def rem_stopwords(string, blacklist=stopwords.words('portuguese')):
+    tokens = tokenizar_string(string)
+
+    filtrados = []
+    for t in tokens:
+        if t.lower() not in blacklist:
+            filtrados.append(t)
+
+    return " ".join(filtrados)
--- teste_pre.py
+++ teste_pre.py
@@ -1,7 +1,14 @@
 import dm.pre_processamento as pp
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 t = RegexpTokenizer('[a-zA-Z]+')
 
 print(pp.tokenizar(["ola mundo", "bora ver no que da", "77777"], t))
 print(pp.tokenizar(["ola mundo", "bora ver no que da", "77777"]))
+
+print(pp.rem_stopwords("esta frase esteja e um teste não maria"))
+
+sw = stopwords.words('portuguese')
+sw.append('maria')
+print(pp.rem_stopwords("esta frase esteja e um teste não maria", sw))
