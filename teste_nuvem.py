c3ad50e636c82044458c11e1a71579e8f1877a34
David Pinheiro <davepinh@gmail.com>
2018-09-17 14:52:21 -0300

    Melhora das stopwords na nuvem de palavas

--- teste_nuvem.py
+++ teste_nuvem.py
@@ -1,11 +1,14 @@
 import dm.nuvem_palavras as nu
 import dm.pre_processamento as pp
+import dm.stopwords as sw
 import os
 
-f = open(os.environ['DATADIR'] + '/MarinaSilva.csv', 'r').read()
+f = open(os.environ['DATADIR'] + '/jairbolsonaro.csv', 'r').read()
 
 res = [ r"\w+:(\/?\/?)[^\s]+",
         r"\#.*\b" ]
 
-n = nu.nuvem(pp.rem_stopwords(pp.rem_regexp(res, str(f))))
+stopwords = sw.portugues + ["RT", "rt"]
+
+n = nu.nuvem(pp.rem_stopwords(pp.rem_regexp(res, str(f)), stopwords))
 nu.mostrar_nuvem(n)
