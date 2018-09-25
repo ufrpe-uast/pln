4e8f55a41c5081e0fbf01f7259047fa61b4a8c7b
David Pinheiro <davepinh@gmail.com>
2018-09-25 13:53:47 -0300

    Mais trabalho feito

--- dm/classificador.py
+++ dm/classificador.py
@@ -1,21 +1,19 @@
-from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# classificar : [string] -> [string] -> string -> ?modelo -> float -> {}
-def classificar(opinioes, polaridades, frase, modelo=MultinomialNB(), ts=.3):
-    # XXX: module cross_validation esta depreciada
+# classificar : [string] -> [string] -> [string] -> ?modelo -> ?vec -> float -> {}
+def classificar(opinioes, polaridades, frase, modelo=MultinomialNB(), vec=TfidfVectorizer(), ts=.3):
     dados_teste, dados_val, pols_teste, pols_val = train_test_split(opinioes, polaridades, test_size=ts)
 
-    bag = CountVectorizer()
-    bag_treino = bag.fit_transform(dados_teste)
-    bag_val = bag.transform(dados_val)
+    bag_treino = vec.fit_transform(dados_teste)
+    bag_val = vec.transform(dados_val)
 
     modelo.fit(bag_treino.toarray(), pols_teste)
 
     pols_pred_val = modelo.predict(bag_val.toarray())
 
-    bag_teste = bag.transform(frase)
+    bag_teste = vec.transform(frase)
 
     pols_pred_teste = modelo.predict(bag_teste.toarray())
 
--- testes.py
+++ testes.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import dm.stopwords as sw
 import dm.pre_processamento as pp
+import dm.classificador as cl
 
 from nltk.util import bigrams
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, RegexpTokenizer
 
-dataset = pd.read_csv('/home/d/data/coleta/ciro.csv', sep='|', usecols=['opiniao', 'polaridade'])
+from sklearn.metrics import accuracy_score
+
+# dataset = pd.read_csv('/home/d/data/coleta/ciro.csv', sep='|', usecols=['opiniao', 'polaridade'])
+dataset = pd.read_csv('/home/d/data/opinioes.csv', usecols=['opiniao', 'polaridade'])
 
 opinioes = dataset['opiniao'].values
 polaridades = dataset['polaridade'].values
 
 # 1
 opinioespp = [pp.rem_stopwords(pp.rem_regexp(pp.regexes, pp.rem_emojis(o)), blacklist=sw.portugues) for o in opinioes]
+
+t = RegexpTokenizer('[\w]+')
+opinioespp = [' '.join(pp.tokenizar_string(o, t)).lower() for o in opinioespp]
 
 # 2
 # ngram (bigram)
@@ -22,3 +29,15 @@ stem = [' '.join(pp.stemming(o)) for o in opinioespp]
 
 # word token
 tokens = [word_tokenize(o) for o in opinioespp]
+
+res = cl.classificar(stem, polaridades, pp.stemming('tá na cara que é o melhor candidato'))
+
+if res['pols_pred_teste'][0] == 1:
+    print('Positivo')
+elif res['pols_pred_teste'][0] == 2:
+    print('Negativo')
+else:
+    print('Invalido')
+
+acc = accuracy_score(res['pols_val'], res['pols_pred_val']) * 100
+print('Acuracia: {0:.2f} %'.format(acc))
